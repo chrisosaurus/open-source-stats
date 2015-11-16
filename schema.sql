f489e81273904f42b21e3bc85e89e5fc13ed999c
Chris Hall <followingthepath@gmail.com>
2015-11-16 22:46:52 +1300

    uncommenting user and user stats in schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 
 pragma foreign_keys=on;
 
---create table user (
---    id      integer primary key autoincrement not null ,
---    email   text                              not null
---);
+create table user (
+    id      integer primary key autoincrement not null ,
+    email   text                              not null
+);
 
 create table project (
     id              integer primary key autoincrement not null ,
@@ -38,23 +38,23 @@ create table project_stats (
     commits_in_period integer                 not null
 );
 
---create table project_user_stats (
---    id           integer primary key autoincrement not null ,
---    project_id   integer                           not null references project(id) ,
---    user_id      integer                           not null references user(id) ,
---    generated    datetime                          not null ,
---    -- month with year reference in iso format: "2015-11"
---    month        text                              not null,
---    -- the hash of origin/<branch> when we generated this entry
---    hash         text                              not null ,
---    -- the hash for the previous entry
---    parent_hash  text                                     default "",
---    -- count of commits to this project up to and including this has
---    -- so this includes the count for parent_hash and parent_hash's parent_hash and so on
---    total_commits integer                     not null,
---    -- count of commits only in this period (so excluding parent hash's count)
---    commits_in_period integer                 not null
---);
+create table project_user_stats (
+    id           integer primary key autoincrement not null ,
+    project_id   integer                           not null references project(id) ,
+    user_id      integer                           not null references user(id) ,
+    generated    datetime                          not null ,
+    -- month with year reference in iso format: "2015-11"
+    month        text                              not null,
+    -- the hash of origin/<branch> when we generated this entry
+    hash         text                              not null ,
+    -- the hash for the previous entry
+    parent_hash  text                                     default "",
+    -- count of commits to this project up to and including this has
+    -- so this includes the count for parent_hash and parent_hash's parent_hash and so on
+    total_commits integer                     not null,
+    -- count of commits only in this period (so excluding parent hash's count)
+    commits_in_period integer                 not null
+);
 
 insert into project (repo_url) values ("https://github.com/CausalityLtd/ponyc.git");
 
