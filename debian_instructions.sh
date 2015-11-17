6a226a6b930b425bd8b96873685d65c2afe00f87
Chris Hall <followingthepath@gmail.com>
2015-11-17 17:02:13 +1300

    adding debian build instructions

---
+++ debian_instructions.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# run me as sudo
+set -c
+
+apt-get update
+apt-get install git build-essential python3 sqlite3 python3-pip
+
+# from http://www.pygit2.org/install.html#quick-install
+wget https://github.com/libgit2/libgit2/archive/v0.23.2.tar.gz
+tar xzf v0.23.2.tar.gz
+cd libgit2-0.23.2/
+cmake .
+make
+make install
+
+pip3 install pygit2
+
