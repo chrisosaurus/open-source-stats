625a1a0e2477e16b815b1e703f1f7806b7862905
Chris Hall <chris.hall@catalyst.net.nz>
2015-11-19 12:31:04 +1300

    adding golang to sample db

--- schema.sql
+++ schema.sql
@@ -59,4 +59,5 @@ create table project_user_stats (
 
 insert into project (repo_url) values ("https://github.com/CausalityLtd/ponyc.git");
 insert into project (repo_url) values ("https://github.com/rust-lang/rust.git");
+insert into project (repo_url) values ("https://github.com/golang/go.git");
 
