e043a7dd4e271f54b627869aedc6ceaec135640f
Chris Hall <followingthepath@gmail.com>
2015-11-18 16:50:25 +1300

    refactoring data generation to allow multi-project

--- site/data.js
+++ site/data.js
@@ -1,16 +1,3 @@
-var dataset_from = "2014-11";
-var dataset_to = "2015-11";
 var dataset = [
-    197,
-    178,
-    148,
-    66,
-    209,
-    227,
-    96,
-    60,
-    29,
-    92,
-    91,
-    73
+    { project: "github.com_CausalityLtd_ponyc.git",  data_from: "2014-11",  data_to: "2015-11",  data : [ 197, 178, 148, 66, 209, 227, 96, 60, 29, 92, 91, 73, ], },
 ];
--- src/generate_data.py
+++ src/generate_data.py
@@ -8,8 +8,8 @@ dbname = "oos.sqlite"
 # period of 12 months
 period = 12
 
-# FIXME harcoded project id
-project_id = 1
+# harcoded project id2
+project_ids = [1]
 
 output_path = "site/data.js"
 
@@ -27,67 +27,87 @@ def prev_month(monstr):
     newmonstr = "%.4d-%.2d" %(year, month)
     return newmonstr
 
-def generate_data(dbname, project_id, period):
+def generate_data(dbname, project_ids, period):
     # create an array <period> in length
     # initially populated with all 0s
-    data = []
-    for i in range(period):
-        data.append(0)
-
-    months = {}
-
-    with sqlite3.connect(dbname) as conn:
-        for row in conn.execute('''select month, commits_in_period from project_stats where project_id = ? order by month desc limit ?''', (project_id, period)):
-            monstr = row[0]
-            commits_in_period = row[1]
-            months[monstr] = commits_in_period
+    data = {}
 
     now = datetime.datetime.now()
     year = now.year
     month = now.month
 
-    monstr = "%.4d-%.2d" %(year, month)
+    with sqlite3.connect(dbname) as conn:
+        for project_id in project_ids:
+            # get project name
+            row = conn.execute('''select name from project where id = ?''', (project_id,))
+            row = row.fetchone()
 
-    initial_date = monstr
+            if row is None:
+                print("Error: failed to find project with id '%d'" %(project_id))
+                return []
 
-    for i in range(period):
-        if monstr in months:
-            # add from back
-            data[-(i+1)] = months[monstr]
-        monstr = prev_month(monstr)
+            project_name = row[0]
 
-    # note this is 'off by one'
-    # this is desirable due to js data handling
-    final_date = monstr
+            project_data = []
+            for i in range(period):
+                project_data.append(0)
 
-    return {
-            "from": final_date,
-            "to": initial_date,
-            "contents": data,
-           }
+            months = {}
+
+            for row in conn.execute('''select month, commits_in_period from project_stats where project_id = ? order by month desc limit ?''', (project_id, period)):
+                monstr = row[0]
+                commits_in_period = row[1]
+                months[monstr] = commits_in_period
+
+        monstr = "%.4d-%.2d" %(year, month)
+        initial_date = monstr
+
+        for i in range(period):
+            if monstr in months:
+                # add from back
+                project_data[-(i+1)] = months[monstr]
+            monstr = prev_month(monstr)
+
+        # note this is 'off by one'
+        # this is desirable due to js data handling
+        final_date = monstr
+
+        data[project_name] = {
+                            "from": final_date,
+                            "to": initial_date,
+                            "contents": project_data,
+                             }
+
+    return data
+
 
 def output_data(data, output_path):
-    dfrom = data["from"]
-    dto = data["to"]
-    contents = data["contents"]
-
     output = []
-
-    output.append( '''var dataset_from = "%s";\n''' %(dfrom) )
-    output.append( '''var dataset_to = "%s";\n''' %(dto) )
     output.append( '''var dataset = [\n''' )
-    for i in range(len(contents)):
-        string = '''    %d''' %(contents[i])
-        if i != (len(contents)-1):
-            string += ","
-        string += "\n"
-        output.append(string)
+
+    for project_name in data.keys():
+        project_data = data[project_name]
+
+        dfrom = project_data["from"]
+        dto = project_data["to"]
+        contents = project_data["contents"]
+
+        output.append( '''    { project: "%s", ''' %(project_name) )
+        output.append( ''' data_from: "%s", ''' %(dfrom) )
+        output.append( ''' data_to: "%s", ''' %(dto) )
+        output.append( ''' data : [ ''' )
+        for i in range(len(contents)):
+            output.append( '''%d, ''' %(contents[i]) )
+        output.append( '''], ''')
+        output.append( '''},\n''' )
+
     output.append( '''];\n''' )
 
     with open(output_path, 'w') as file:
         file.writelines(output)
 
 if __name__ == "__main__":
-    data = generate_data(dbname, project_id, period)
+
+    data = generate_data(dbname, project_ids, period)
     output_data(data, output_path)
 
